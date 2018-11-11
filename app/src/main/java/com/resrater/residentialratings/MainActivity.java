a2953f39b1e5eff3e0730f59515e10a01f2dd8f2
Charles Durfee <charlesdurfee@gmail.com>
2018-11-11 15:44:36 -0700

    converted maps activity to maps fragment

--- app/src/main/java/com/resrater/residentialratings/MainActivity.java
+++ app/src/main/java/com/resrater/residentialratings/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,8 +23,13 @@ public class MainActivity extends AppCompatActivity {
         btnMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-                startActivity(intent);
+                setContentView(R.layout.activity_maps);
+                MapsActivity mapsFragment = new MapsActivity();
+                FragmentManager fm = getSupportFragmentManager();
+                fm.beginTransaction()
+                        .replace(R.id.map, mapsFragment,"mapsFrag")
+                        .addToBackStack(null)
+                        .commit();
             }
         });
     }
--- app/src/main/java/com/resrater/residentialratings/MapsActivity.java
+++ app/src/main/java/com/resrater/residentialratings/MapsActivity.java
@@ -2,8 +2,15 @@ package com.resrater.residentialratings;
 
 import android.location.Address;
 import android.location.Geocoder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,39 +27,61 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends Fragment implements OnMapReadyCallback {
 
+    private View root;
+    private SupportMapFragment mapFragment;
     private static final float DEFAULT_ZOOM = 19;
     private GoogleMap mMap;
     private LatLng latLng, home;
     private Marker marker, homeMarker;
     Geocoder geocoder;
 
+    public MapsActivity() {
+
+    }
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        FragmentManager fm = getChildFragmentManager();
+
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance();
+            fm.beginTransaction().replace(R.id.map, mapFragment).commit();
+        }
+
+
+
+        return root = inflater.inflate(R.layout.activity_maps, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         // geocoder turns a latlng into a human address
-        geocoder = new Geocoder(this, Locale.US);
+        geocoder = new Geocoder(this.getContext(), Locale.US);
 
         // start the map at the users home
         home = new LatLng(40.647360, -112.306890);
 
     }
 
-     // Manipulates the map once available.
+    // Manipulates the map once available.
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
         //mMap.getUiSettings().setMyLocationButtonEnabled(true);
 
         // move the map to the users home and add a marker
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(home));
+        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(home , 19.0f) );
         homeMarker = mMap.addMarker(new MarkerOptions().position(home).title("Home").draggable(true)
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_home_blue)));
 
@@ -73,7 +102,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 android.location.Address address = addresses.get(0);
                 if (address != null) {
-                    Toast.makeText(MapsActivity.this, address.getFeatureName() +" "
+                    Toast.makeText(MapsActivity.this.getContext(), address.getFeatureName() +" "
                             + address.getThoroughfare() +" " +address.getPostalCode(), Toast.LENGTH_SHORT).show();
                 }
 
