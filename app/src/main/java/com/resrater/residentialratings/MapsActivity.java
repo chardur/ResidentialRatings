335f9dedb158bb32b53f2d4cd2bea334b8aa74f0
Charles Durfee <charlesdurfee@gmail.com>
2018-11-10 20:04:09 -0700

    added function for user to select map location

--- app/src/main/java/com/resrater/residentialratings/MapsActivity.java
+++ app/src/main/java/com/resrater/residentialratings/MapsActivity.java
@@ -1,18 +1,31 @@
 package com.resrater.residentialratings;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final float DEFAULT_ZOOM = 19;
     private GoogleMap mMap;
+    private LatLng latLng, home;
+    private Marker marker, homeMarker;
+    Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +35,55 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // gecoder turns a latlng into a human address
+        geocoder = new Geocoder(this, Locale.US);
+
+        // start the map at the users home
+        home = new LatLng(40.647360, -112.306890);
+
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+     // Manipulates the map once available.
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // move the map to the users home and add a marker
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(home));
+        homeMarker = mMap.addMarker(new MarkerOptions().position(home).title("Home"));
+
+        // listener for user to select locations on the map
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng point) {
+                //save current location
+                latLng = point;
+
+                List<Address> addresses = new ArrayList<>();
+                try {
+                    addresses = geocoder.getFromLocation(point.latitude, point.longitude,1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                android.location.Address address = addresses.get(0);
+                if (address != null) {
+                    Toast.makeText(MapsActivity.this, address.getFeatureName() +" "
+                            + address.getThoroughfare() +" " +address.getPostalCode(), Toast.LENGTH_SHORT).show();
+                }
+
+                //remove previously placed Marker
+                if (marker != null) {
+                    marker.remove();
+                }
+
+                //place marker where user just clicked
+                marker = mMap.addMarker(new MarkerOptions().position(point).title("Marker"));
+
+            }
+        });
+
     }
 }
